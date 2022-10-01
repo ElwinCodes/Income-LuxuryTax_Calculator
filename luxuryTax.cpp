747ab89b3dde9a9f80afcc8649c5fcbda7ff77e6
Elwin Liu <elwinzrliu@gmail.com>
2022-09-30 23:35:51 -0700

    added new functions to luxury tax

--- luxuryTax.cpp
+++ luxuryTax.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include "luxuryTax.hpp"
 using namespace std;
+
+luxuryTax::luxuryTax(double price){
+    priceofobject = price;
+}
+
+luxuryTax::luxuryTax(){
+    priceofobject = 0;
+}
+
+void luxuryTax::input(){
+    cout << "Please enter in the price of the object: " << endl;
+    cin >> priceofobject;
+}
+
+void luxuryTax::calculate(){
+    //something here 
+}
+void luxuryTax::output(){
+    //something here 
+}
--- luxuryTax.hpp
+++ luxuryTax.hpp
@@ -1,5 +1,22 @@
 #ifndef LUXURYTAX_H
 #define LUXURYTAX_H
 
+class luxuryTax{
+    public:
+    luxuryTax(double price);
+    luxuryTax();
+    void input();
+    void output();
+    int getPrice();
+    void setPrice();
+    void calculate();
+    
+    private:
+    double priceofobject;
+
+
+};
+
+
 
 #endif
